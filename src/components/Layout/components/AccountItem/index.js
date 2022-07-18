047e67a079edd0f3c58cf13b1a80ba5ee2e6944c
Eric <ericta1911@gmail.com>
2022-07-18 10:10:02 +0700

    custom header #3

---
+++ src/components/Layout/components/AccountItem/index.js
@@ -0,0 +1,24 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from './AccountItem.module.scss'
+
+const cx = classNames.bind(styles)
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/b4f45ab45c999aac225a7322dc33237f~c5_100x100.jpeg?biz_tag=tiktok_user.user_cover&x-expires=1658152800&x-signature=TkTF5Y9RiaGhpDCeKY2CnL9SwGU%3D" alt="" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default AccountItem;
